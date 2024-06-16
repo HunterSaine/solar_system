82fda2264b7604842a188fdcc0c1ca2bcebd711a
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-16 17:06:08 -0400

    created scene, camera, and renderer in main.js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1,1000);
+const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg')});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene,camera)
 
 
 const controls = new OrbitControls( camera, renderer.domElement );
