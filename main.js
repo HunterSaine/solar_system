662260f58bc49d3a6f9a6b8484c421205d313c3f
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-16 17:37:11 -0400

    edit SUN geometry

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ camera.position.setZ(30);
 renderer.render(scene,camera);
 
 //create sun model
-const geometery = new THREE.SphereGeometry(10,100,100)
+const geometery = new THREE.SphereGeometry(10,50,50)
 const material = new THREE.MeshStandardMaterial({color:0xfdb813, wireframe:true})
 const SUN = new THREE.Mesh(geometery,material);
 scene.add(SUN);
