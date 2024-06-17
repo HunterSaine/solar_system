fbc4bda1580ec264c03195e322312ae3007d0e0d
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-17 18:46:19 -0400

    edit path for mercury texture

--- planets/mercury.js
+++ planets/mercury.js
@@ -4,7 +4,7 @@ export{MERCURY,mercurySystem, rotateMercury, orbitMercury};
 const TextureLoader = new THREE.TextureLoader();
 
 const mercuryGeometry = new THREE.SphereGeometry(3,50,50)
-const mercuryMaterial = new THREE.MeshStandardMaterial({map: TextureLoader.load('/planets/textures/mercurymap.webp')});
+const mercuryMaterial = new THREE.MeshStandardMaterial({map: TextureLoader.load('/textures/mercurymap.webp')});
 const MERCURY = new THREE.Mesh(mercuryGeometry,mercuryMaterial);
 const mercurySystem = new THREE.Object3D();
 
