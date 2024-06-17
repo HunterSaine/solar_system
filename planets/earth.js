8a97294065c38983cf7cd954b9944e6c129bed50
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-17 18:55:16 -0400

    changed file structure

--- planets/earth.js
+++ planets/earth.js
@@ -4,7 +4,7 @@ export{EARTH, earthSystem, rotateEarth, orbitEarth};
 const TextureLoader = new THREE.TextureLoader();
 
 const earthGeometry = new THREE.SphereGeometry(5,50,50)
-const earthMaterial = new THREE.MeshStandardMaterial({map: TextureLoader.load('./planets/textures/earthmap.jpeg')});
+const earthMaterial = new THREE.MeshStandardMaterial({map: TextureLoader.load('/textures/earthmap.jpeg')});
 const EARTH = new THREE.Mesh(earthGeometry,earthMaterial);
 const earthSystem = new THREE.Object3D();
 
--- planets/venus.js
+++ planets/venus.js
@@ -4,7 +4,7 @@ export{VENUS, venusSystem, rotateVenus, orbitVenus};
 const TextureLoader = new THREE.TextureLoader();
 
 const venusGeometry = new THREE.SphereGeometry(5,50,50)
-const venusMaterial = new THREE.MeshStandardMaterial({map: TextureLoader.load('./planets/textures/venusmap.jpeg')});
+const venusMaterial = new THREE.MeshStandardMaterial({map: TextureLoader.load('/textures/venusmap.jpeg')});
 const VENUS = new THREE.Mesh(venusGeometry, venusMaterial);
 const venusSystem = new THREE.Object3D();
 
