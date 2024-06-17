acb9938bf45f561619b0be3b7feac45a0ac51fec
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-16 21:20:03 -0400

    added textures folder

--- planets/earth.js
+++ planets/earth.js
@@ -4,7 +4,7 @@ export{EARTH, rotateEarth};
 const TextureLoader = new THREE.TextureLoader();
 
 const geometry = new THREE.SphereGeometry(2,50,50)
-const material = new THREE.MeshStandardMaterial({map: TextureLoader.load('../earthmap.jpeg')});
+const material = new THREE.MeshStandardMaterial({map: TextureLoader.load('./textures/earthmap.jpeg')});
 const EARTH = new THREE.Mesh(geometry,material);
 
 function rotateEarth(){
--- planets/sun.js
+++ planets/sun.js
@@ -5,7 +5,7 @@ export{SUN, SOLARSYSTEM, rotateSun}
 const TextureLoader = new THREE.TextureLoader();
 
 const geometry = new THREE.SphereGeometry(10,50,50)
-const material = new THREE.MeshStandardMaterial({map: TextureLoader.load('../sunmap.jpg')})
+const material = new THREE.MeshStandardMaterial({map: TextureLoader.load('./textures/sunmap.jpg')})
 const SUN = new THREE.Mesh(geometry,material);
 
 const SOLARSYSTEM = new THREE.Mesh(geometry, material);
