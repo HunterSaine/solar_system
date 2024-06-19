ca3a112afb7463d3c497a2ee0720d055428d1caa
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-18 20:56:24 -0400

    added mars texture and made planet positions closer to the sun

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import{SUN, rotateSun} from './planets/sun.js'
-import{EARTH, earthSystem, rotateEarth, orbitEarth} from './planets/earth.js'
-import{ambientLight, pointLight} from './light.js'
-import { MERCURY, rotateMercury, orbitMercury, mercurySystem} from './planets/mercury.js';
+import { SUN, rotateSun } from './planets/sun.js'
+import { EARTH, earthSystem, rotateEarth, orbitEarth } from './planets/earth.js'
+import { ambientLight, pointLight } from './light.js'
+import { MERCURY, rotateMercury, orbitMercury, mercurySystem } from './planets/mercury.js';
 import { VENUS, venusSystem, orbitVenus, rotateVenus } from './planets/venus.js';
-import { MARS,marsSystem,orbitMars,rotateMars } from './planets/mars.js';
+import { MARS, marsSystem, orbitMars, rotateMars } from './planets/mars.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1,1000);
-const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg')});
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#bg') });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -17,21 +17,21 @@ camera.position.setZ(120);
 camera.position.setY(80);
 camera.position.setX(-70);
 
-renderer.render(scene,camera);
+renderer.render(scene, camera);
 
-scene.add(SUN,earthSystem, mercurySystem, venusSystem, marsSystem);
+scene.add(SUN, earthSystem, mercurySystem, venusSystem, marsSystem);
 earthSystem.add(EARTH);
 venusSystem.add(VENUS);
 mercurySystem.add(MERCURY);
 marsSystem.add(MARS)
 
 //positions
-MERCURY.position.x=50;
-MERCURY.position.z=-50
-VENUS.position.x=-100;
-EARTH.position.x= 200;
-MARS.position.x=240;
-MARS.position.y=240;
+MERCURY.position.x = 50;
+MERCURY.position.z = -50
+VENUS.position.x = -100;
+EARTH.position.x = 150;
+MARS.position.x = 200;
+MARS.position.z = -200;
 
 
 
@@ -43,11 +43,11 @@ scene.add(pointLight);
 //scene.add(gridHelper);
 
 //adds controls to move around the scene
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 
-const EARTHYEAR = (2 * Math.PI * (1/60) * (1/60))/2;
-const EARTHDAY = (EARTHYEAR * 365); 
-function animate(){
+const EARTHYEAR = (2 * Math.PI * (1 / 60) * (1 / 60)) / 2;
+const EARTHDAY = (EARTHYEAR * 365);
+function animate() {
   requestAnimationFrame(animate);
   rotateSun();
   //earth orbits the sun one cycle per minute
@@ -55,7 +55,7 @@ function animate(){
   rotateEarth(EARTHDAY);
   orbitMercury(EARTHYEAR);
   rotateMercury(EARTHYEAR);
-  orbitVenus(EARTHYEAR/.615);
+  orbitVenus(EARTHYEAR / .615);
   rotateVenus(EARTHYEAR);
   rotateMars(EARTHDAY);
   orbitMars(EARTHYEAR);
--- planets/mars.js
+++ planets/mars.js
@@ -4,7 +4,7 @@ export { MARS, marsSystem, rotateMars, orbitMars };
 const TextureLoader = new THREE.TextureLoader();
 
 const marsGeometry = new THREE.SphereGeometry(5, 50, 50)
-const marsMaterial = new THREE.MeshStandardMaterial({ map: TextureLoader.load('/textures/venusmap.jpeg') });
+const marsMaterial = new THREE.MeshStandardMaterial({ map: TextureLoader.load('/textures/marsmap.jpeg') });
 const MARS = new THREE.Mesh(marsGeometry, marsMaterial);
 const marsSystem = new THREE.Object3D();
 
@@ -12,5 +12,5 @@ function rotateMars(EARTHDAY) {
     MARS.rotation.y += EARTHDAY;
 }
 function orbitMars(EARTHYEAR) {
-    marsSystemSystem.rotation.y += EARTHYEAR * 0.615;
+    marsSystem.rotation.y += EARTHYEAR / 1.88;
 }
